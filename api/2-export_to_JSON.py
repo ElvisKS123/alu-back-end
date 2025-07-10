c71d8e360be530c30e398365e01d196001737d6d
ElvisKS123 <e.shimwa2@alustudent.com>
2025-07-10 21:22:16 +0200

    Api DONE

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,52 +1,25 @@
 #!/usr/bin/python3
-"""
-Exports data of an employee's TODO list to a JSON file.
-format: {"USER_ID": [{"task": ..., "completed": ..., "username": ...}, ...]}
-"""
+''' Test request to parse API's
+'''
+import csv
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 2-export_to_JSON.py <employee_id>")
-        sys.exit(1)
-
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer")
-        sys.exit(1)
-
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = "{}/users/{}".format(base_url, employee_id)
-    todos_url = "{}/todos?userId={}".format(base_url, employee_id)
-
-    # Get user info
-    user_response = requests.get(user_url)
-    if user_response.status_code != 200:
-        print("Employee not found")
-        sys.exit(1)
-
-    user_data = user_response.json()
-    username = user_data.get("username")
-
-    # Get all tasks
-    todos_response = requests.get(todos_url)
-    todos = todos_response.json()
-
-    # Prepare JSON Structure
-    task_list = []
-    for task in todos:
-        task_list.append({
-            "task": task.get("title"),
-            "completed": task.get("completed"),
-            "username": username
-        })
-
-    data = {str(employee_id): task_list}
-
-    # save to file
-    filename = "{}.json".format(employee_id)
-    with open(filename, "w") as jsonfile:
-        json.dump(data, jsonfile)
+    if len(sys.argv) > 1 and sys.argv[1].isdigit():
+        api_endpoint = "https://jsonplaceholder.typicode.com"
+        user_id = sys.argv[1]
+        user_data = requests.get(api_endpoint + "/users/" + user_id).json()
+        username = user_data.get('username')
+        todo_data = \
+            requests.get(api_endpoint + "/users/" + user_id + "/todos").\
+            json()
+        with open("{}.json".format(user_id), 'w') as json_file:
+            tasks = []
+            for task in todo_data:
+                tasks.append({'task': task['title'],
+                              'completed': task['completed'],
+                              'username': username})
+            data = {"{}".format(user_id): tasks}
+            json.dump(data, json_file)
